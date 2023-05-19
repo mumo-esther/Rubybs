ed7dd26650e41d988217474434533d47bdf2bb68
mumo-esther <mumoesther2021@gmail.com>
2023-05-19 15:51:00 +0300

    Add a bug

--- MyEnumerable.rb
+++ MyEnumerable.rb
@@ -9,7 +9,25 @@ module MyEnumerable
     false
   end
 
-  def filter(&block)
+  def filter(&block)module MyEnumerable
+    def all?(&block)
+      each { |element| return false unless yield(element) }
+      true
+    end
+  
+    def any?(&block)
+      each { |element| return true if yield(element) }
+      false
+    end
+  
+    def filter(&block)
+      # Bug: result is declared outside the method, making it shared among invocations
+      @result ||= []
+      each { |element| @result << element if yield(element) }
+      @result
+    end
+  end
+  
     result = []
     each { |element| result << element if yield(element) }
     result
